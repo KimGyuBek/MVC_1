28f47bd06759651b144184f925e9b5b730be28cb
rlarbqor00 <rlarbqor00@naver.com>
2022-08-01 15:22:45 +0900

    [add]create frontControllerV2

---
+++ src/main/java/hello/serverlet/web/frontcontroller/v2/ControllerV2.java
@@ -0,0 +1,16 @@
+package hello.serverlet.web.frontcontroller.v2;
+
+import hello.serverlet.web.frontcontroller.MyView;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.rowset.serial.SerialException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException;
+
+}
+
